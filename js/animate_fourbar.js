579aa73b0b64bcb14a999b78406b39e198678074
Dillon Huff <dillonhuff@gmail.com>
2017-02-21 22:02:55 -0800

    Now adding animation adjustments

--- js/animate_fourbar.js
+++ js/animate_fourbar.js
@@ -1,38 +1,39 @@
 
 if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 
+
+var FizzyText = function() {
+    this.motorPhase = Math.PI / 150;
+    this.crankLength = 0.7;
+    this.bdLength = 1.5;
+    this.cdLength = 1.4;
+};
+
+
+var text = new FizzyText();
+
+window.onload = function() {
+    var gui = new dat.GUI();
+    gui.add(text, 'motorPhase', 0.0, Math.PI / 10.0);
+    gui.add(text, 'crankLength', 0.001, 5.0);
+    gui.add(text, 'bdLength', 0.001, 5.0);
+    gui.add(text, 'cdLength', 0.001, 5.0);
+};
+
+
 var container;
 
-// NOTE: cameraTarget will not be needed once trackball controls are used
 var camera, cameraTarget, scene, renderer, controls;
 
 var theta_2, phase_inc;
 var lineGeom, lineMat, stockLines, couplerLine, couplerCurveLines;
-
-var ac_len = 0.7; //0.3; //1.0; //0.5;
-var bd_len = 1.5; //0.75;
-var cd_len = 1.4; //2.0;
-
-var quad =
-    new Quadrilateral(new THREE.Vector3(0, 0, 0),
-		      new THREE.Vector3(1.0, 0, 0),
-		      ac_len,
-		      bd_len,
-		      cd_len
-		     );
-
-if (!isGreshof( quad ) ) {
-    alert('Non greshof linkage!');
-}
-
-var fbl = new FourbarLinkage(quad, 0.7, Math.PI / 3.0);
 
 init();
 animate();
 
 function init() {
     theta_2 = 0;
-    phase_inc = Math.PI / 150;
+    //phase_inc = Math.PI / 150;
 
     lineGeom = new THREE.Geometry();
     couplerGeom = new THREE.Geometry();
@@ -56,17 +57,6 @@ function init() {
     camera = new THREE.PerspectiveCamera( 35, window.innerWidth / window.innerHeight, 1, 15 );
     camera.position.set( 0, 0, 6 );
 
-    // controls = new THREE.TrackballControls( camera );
-    // controls.rotateSpeed = 3.0;
-    // controls.zoomSpeed = 1.2;
-    // controls.panSpeed = 0.8;
-    // controls.noZoom = false;
-    // controls.noPan = false;
-    // controls.staticMoving = true;
-    // controls.dynamicDampingFactor = 0.0;
-    // controls.keys = [ 65, 83, 68 ];
-    // controls.addEventListener( 'change', render );
-
     scene = new THREE.Scene();
 
     scene.add(stockLines);
@@ -87,17 +77,16 @@ function init() {
     //addLine(scene, theta_2);
 
     // Initial curve
-    
-    var tht = 0.0;
-    while (tht < 2*Math.PI) {
-	couplerCurveLines.geometry.vertices.push( fbl.solveCDEPos(tht)[2] );
-	tht += phase_inc;
-    }
+    // var tht = 0.0;
+    // var tht_inc = Math.PI / 30.0;
+    // while (tht < 2*Math.PI) {
+    // 	couplerCurveLines.geometry.vertices.push( fbl.solveCDEPos(tht)[2] );
+    // 	tht += tht_inc;
+    // }
 
     couplerCurveLines.geometry.verticesNeedUpdate = true;
 
     // renderer
-
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setClearColor( fog_color );
     renderer.setPixelRatio( window.devicePixelRatio );
@@ -145,7 +134,6 @@ function onWindowResize() {
     camera.updateProjectionMatrix();
 
     renderer.setSize( window.innerWidth, window.innerHeight );
-    //controls.handleResize();
     render();
     
 }
@@ -154,14 +142,13 @@ function animate() {
 
     requestAnimationFrame( animate );
 
-    addLine(scene, fbl, theta_2);
-    theta_2 = theta_2 + phase_inc;
+    addLine(scene, theta_2);
+    theta_2 = theta_2 + text.motorPhase; //phase_inc;
     if (theta_2 > 2*Math.PI) {
 	theta_2 = theta_2 - 2*Math.PI;
     }
 
     render();
-    //controls.update();
 
 }
 
@@ -171,8 +158,27 @@ function render() {
 
 }
 
-function addLine(sc, fbl, th) {
+function addLine(sc, th) {
 
+    var ac_len = 0.7; //0.3; //1.0; //0.5;
+    var bd_len = 1.5; //0.75;
+    var cd_len = 1.4; //2.0;
+
+    var quad =
+	new Quadrilateral(new THREE.Vector3(0, 0, 0),
+			  new THREE.Vector3(1.0, 0, 0),
+			  text.crankLength,
+			  text.bdLength,
+			  text.cdLength
+			 );
+
+    if (!isGreshof( quad ) ) {
+	alert('Non greshof linkage!');
+    }
+
+    var fbl = new FourbarLinkage(quad, 0.7, Math.PI / 3.0);
+
+    
     var a = fbl.aPos(); //quad);
     var b = fbl.bPos();  //quad);
 
@@ -198,18 +204,3 @@ function addLine(sc, fbl, th) {
     couplerLines.geometry.verticesNeedUpdate = true;
 
 }
-
-var FizzyText = function() {
-  this.message = 'dat.gui';
-  this.speed = 0.8;
-  this.displayOutline = false;
-};
-
-window.onload = function() {
-  var text = new FizzyText();
-  var gui = new dat.GUI();
-  gui.add(text, 'message');
-  gui.add(text, 'speed', -5, 5);
-  gui.add(text, 'displayOutline');
-  gui.add(text, 'explode');
-};
